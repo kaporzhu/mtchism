c3f735d54cb3d7b4bbc098d472886766257fbec5
Kapor <smokingkapor@gmail.com>
2014-04-02 07:25:46 +0800

    Add meal models

---
+++ meals/migrations/0001_initial.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Dish'
+        db.create_table(u'meals_dish', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=256)),
+            ('creator', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'meals', ['Dish'])
+
+        # Adding model 'DishFoods'
+        db.create_table(u'meals_dishfoods', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('dish', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['meals.Dish'])),
+            ('food', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['foods.Food'])),
+            ('weight', self.gf('django.db.models.fields.FloatField')()),
+            ('creator', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'meals', ['DishFoods'])
+
+        # Adding model 'Meal'
+        db.create_table(u'meals_meal', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=128)),
+            ('creator', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('created_at', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'meals', ['Meal'])
+
+        # Adding M2M table for field dishes on 'Meal'
+        m2m_table_name = db.shorten_name(u'meals_meal_dishes')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('meal', models.ForeignKey(orm[u'meals.meal'], null=False)),
+            ('dish', models.ForeignKey(orm[u'meals.dish'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['meal_id', 'dish_id'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Dish'
+        db.delete_table(u'meals_dish')
+
+        # Deleting model 'DishFoods'
+        db.delete_table(u'meals_dishfoods')
+
+        # Deleting model 'Meal'
+        db.delete_table(u'meals_meal')
+
+        # Removing M2M table for field dishes on 'Meal'
+        db.delete_table(db.shorten_name(u'meals_meal_dishes'))
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'foods.category': {
+            'Meta': {'object_name': 'Category'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        },
+        u'foods.food': {
+            'Meta': {'object_name': 'Food'},
+            'carbohydrate': ('django.db.models.fields.FloatField', [], {'default': '-1'}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['foods.Category']"}),
+            'cellulose': ('django.db.models.fields.FloatField', [], {'default': '-1'}),
+            'fat': ('django.db.models.fields.FloatField', [], {'default': '-1'}),
+            'heat': ('django.db.models.fields.FloatField', [], {'default': '-1'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'protein': ('django.db.models.fields.FloatField', [], {'default': '-1'})
+        },
+        u'meals.dish': {
+            'Meta': {'object_name': 'Dish'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '256'})
+        },
+        u'meals.dishfoods': {
+            'Meta': {'object_name': 'DishFoods'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            'dish': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['meals.Dish']"}),
+            'food': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['foods.Food']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'weight': ('django.db.models.fields.FloatField', [], {})
+        },
+        u'meals.meal': {
+            'Meta': {'object_name': 'Meal'},
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            'dishes': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['meals.Dish']", 'symmetrical': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '128'})
+        }
+    }
+
+    complete_apps = ['meals']
---
+++ meals/models.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from django.contrib.auth.models import User
+from django.db import models
+
+from foods.models import Food
+
+
+class Dish(models.Model):
+    """
+    Each meal has several dishes
+    """
+    name = models.CharField(max_length=256)
+
+    creator = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class DishFoods(models.Model):
+    """
+    Foods in a dish
+    """
+    dish = models.ForeignKey(Dish)
+    food = models.ForeignKey(Food)
+    weight = models.FloatField()  # gram
+
+    creator = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Meal(models.Model):
+    """
+    Each meal in a day.
+    """
+    name = models.CharField(max_length=128)
+    dishes = models.ManyToManyField(Dish)
+
+    creator = models.ForeignKey(User)
+    created_at = models.DateTimeField(auto_now_add=True)
