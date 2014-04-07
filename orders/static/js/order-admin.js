6d89e8f6a04a251cbe2a2bab2155cc5da9f15324
Kapor <smokingkapor@gmail.com>
2014-04-07 08:30:24 +0800

    Fix select all orders bug

--- orders/static/js/order-admin.js
+++ orders/static/js/order-admin.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
+    // bind datetime picker
     $('input.datetime').datetimepicker();
+
+    // highlight the selected orders
+    $('#orders .select-row-btn').change(function(){
+        $(this).parents('tr').toggleClass('selected');
+    });
+
+    // highlight the all orders
+    $('#select-all-btn').change(function(){
+        var checked = $(this).is(':checked');
+        if (checked) {
+            $('table tbody tr input:checkbox:not(:checked)').trigger('click');
+        } else {
+            $('table tbody tr input:checkbox:checked').trigger('click');
+        }
+    });
+
 });
